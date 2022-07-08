d56df20226a8538e59fe54dff2540db9773ec2a1
Ivan A Kireev <ivkireev@yandex.ru>
2022-07-08 16:28:29 +0300

    scenario_baselines_supervised sync

--- scenario_gender/bin/scenario_baselines_supervised.sh
+++ scenario_gender/bin/scenario_baselines_supervised.sh
@@ -1,5 +1,5 @@
 # Train a supervised model and save scores to the file
-python -m ptls.pl_fit_target --config-dir conf --config-name pl_fit_target trainer.max_epochs=3
+python -m ptls.pl_fit_target --config-dir conf --config-name pl_fit_target
 
 # Fine tune the MeLES model in supervised mode and save scores to the file
 python -m ptls.pl_train_module \
@@ -8,28 +8,26 @@ python -m ptls.pl_train_module \
   +pl_module.loss.pair_selector='{_target_: ptls.frames.coles.sampling_strategies.HardNegativePairSelector, neg_count: 5}' \
   ~pl_module.loss.sampling_strategy \
   model_path="models/mles_model_for_finetuning.p" \
-  trainer.max_epochs=3 \
   --config-dir conf --config-name mles_params
 
-python -m ptls.pl_fit_target --config-dir conf --config-name pl_fit_finetuning_mles trainer.max_epochs=3
+python -m ptls.pl_fit_target --config-dir conf --config-name pl_fit_finetuning_mles
 
 # Fine tune the CPC model in supervised mode and save scores to the file
-python -m ptls.pl_fit_target --config-dir conf --config-name pl_fit_finetuning_cpc trainer.max_epochs=3
+python -m ptls.pl_fit_target --config-dir conf --config-name pl_fit_finetuning_cpc
 
 # Fine tune the RTD model in supervised mode and save scores to the file
-python -m ptls.pl_fit_target --config-dir conf --config-name pl_fit_finetuning_rtd trainer.max_epochs=3
+python -m ptls.pl_fit_target --config-dir conf --config-name pl_fit_finetuning_rtd
 
 # Fine tune the MeLES model in supervised mode and save scores to the file
 python -m ptls.pl_train_module \
   pl_module.seq_encoder.hidden_size=256 \
   model_path="models/barlow_twins_model_for_finetuning.p" \
-  trainer.max_epochs=3 \
   --config-dir conf --config-name barlow_twins_params
 # Fine tune the RTD model in supervised mode and save scores to the file
-python -m ptls.pl_fit_target --config-dir conf --config-name pl_fit_finetuning_barlow_twins trainer.max_epochs=3
+python -m ptls.pl_fit_target --config-dir conf --config-name pl_fit_finetuning_barlow_twins
 
-# Compare
-# rm results/scenario_gender_baselines_supervised.txt
-# # rm -r conf/embeddings_validation.work/
-# python -m embeddings_validation \
-#     --config-dir conf --config-name embeddings_validation_baselines_supervised +workers=10 +total_cpu_count=20
+Compare
+rm results/scenario_gender_baselines_supervised.txt
+# rm -r conf/embeddings_validation.work/
+python -m embeddings_validation \
+    --config-dir conf --config-name embeddings_validation_baselines_supervised +workers=10 +total_cpu_count=20
--- scenario_gender/bin/scenario_baselines_unsupervised.sh
+++ scenario_gender/bin/scenario_baselines_unsupervised.sh
@@ -5,34 +5,32 @@ python -m ptls.pl_inference    --config-dir conf --config-name agg_features_para
 python -m ptls.pl_inference    --config-dir conf --config-name random_params
 
 # Train the MeLES encoder and take embedidngs; inference
-python -m ptls.pl_train_module --config-dir conf --config-name mles_params trainer.max_epochs=3
+python -m ptls.pl_train_module --config-dir conf --config-name mles_params
 python -m ptls.pl_inference    --config-dir conf --config-name mles_params
 
 # Train the Contrastive Predictive Coding (CPC) model; inference
-python -m ptls.pl_train_module --config-dir conf --config-name cpc_params trainer.max_epochs=3
+python -m ptls.pl_train_module --config-dir conf --config-name cpc_params
 python -m ptls.pl_inference    --config-dir conf --config-name cpc_params
 
 # Train the Sequence Order Prediction (SOP) model; inference
-python -m ptls.pl_train_module --config-dir conf --config-name sop_params trainer.max_epochs=3
+python -m ptls.pl_train_module --config-dir conf --config-name sop_params
 python -m ptls.pl_inference    --config-dir conf --config-name sop_params
 
 # Train the Next Sequence Prediction (NSP) model; inference
-python -m ptls.pl_train_module --config-dir conf --config-name nsp_params trainer.max_epochs=3
+python -m ptls.pl_train_module --config-dir conf --config-name nsp_params
 python -m ptls.pl_inference    --config-dir conf --config-name nsp_params
 
 # Train the Replaced Token Detection (RTD) model; inference
-python -m ptls.pl_train_module --config-dir conf --config-name rtd_params trainer.max_epochs=3
+python -m ptls.pl_train_module --config-dir conf --config-name rtd_params
 python -m ptls.pl_inference    --config-dir conf --config-name rtd_params
 
 # Check COLEs with split_count=2
-# was
 python -m ptls.pl_train_module \
-    data_module.train.split_strategy.split_count=2 \
-    data_module.valid.split_strategy.split_count=2 \
+    data_module.train_data.splitter.split_count=2 \
+    data_module.valid_data.splitter.split_count=2 \
     pl_module.validation_metric.K=1 \
     model_path="models/mles_model2.p" \
     logger_name="mles_model2" \
-    trainer.max_epochs=3 \
     --config-dir conf --config-name mles_params
 python -m ptls.pl_inference \
     model_path="models/mles_model2.p" \
@@ -40,11 +38,11 @@ python -m ptls.pl_inference \
     --config-dir conf --config-name mles_params
 
 # Train the Contrastive Predictive Coding (CPC) model; inference
-python -m ptls.pl_train_module --config-dir conf --config-name barlow_twins_params trainer.max_epochs=3
+python -m ptls.pl_train_module --config-dir conf --config-name barlow_twins_params
 python -m ptls.pl_inference    --config-dir conf --config-name barlow_twins_params
 
-# Compare
-# rm results/scenario_gender_baselines_unsupervised.txt
-# # rm -r conf/embeddings_validation.work/
-# python -m embeddings_validation \
-#     --config-dir conf --config-name embeddings_validation_baselines_unsupervised +workers=10 +total_cpu_count=20
+Compare
+rm results/scenario_gender_baselines_unsupervised.txt
+# rm -r conf/embeddings_validation.work/
+python -m embeddings_validation \
+    --config-dir conf --config-name embeddings_validation_baselines_unsupervised +workers=10 +total_cpu_count=20
