3c06810eb2460993f5d9877b62a36de7be890aa6
Alexander <mazitovs@gmail.com>
2022-11-17 12:06:06 +0300

    MLMNSP experiment

--- scenario_alpha_battle/bin/scenario_mlmnsp.sh
+++ scenario_alpha_battle/bin/scenario_mlmnsp.sh
@@ -1,15 +1,15 @@
 ulimit -n 32000
-
-tuning_set=\
-"400 4 8 0.00005 512 64 4 64000"
 export device=0
 
+tuning_set=\
+"640 8 8 0.00005 512 64 2 64000"
 while IFS=' ' read -r hidden_size n_heads n_layers max_lr max_len batch_size accumulate_grad_batches max_steps
 do 
-    export logger_name="$hidden_size-$n_heads-$n_layers-$max_lr-$max_len-$batch_size-$accumulate_grad_batches-$max_steps"
+    export logger_name="mlmnsp--$hidden_size-$n_heads-$n_layers-$max_lr-$max_len-$batch_size-$accumulate_grad_batches-$max_steps"
+
     python -m ptls.pl_train_module \
         logger_name=${logger_name} \
-        trainer.gpus=${device} \
+        trainer.gpus=[${device}] \
         pl_module.hidden_size=${hidden_size} \
         pl_module.max_lr=${max_lr} \
         pl_module.seq_encoder.num_attention_heads=${n_heads} \
@@ -20,19 +20,26 @@ do
         data_module.valid_batch_size=${batch_size} \
         trainer.accumulate_grad_batches=${accumulate_grad_batches} \
         trainer.max_steps=${max_steps} \
-        model_path="${hydra:runtime.cwd}/../../artifacts/scenario_alpha_battle/mlmnsp__$logger_name.p" \
+        model_path="models/mlmnsp__$logger_name.p" \
       --config-dir conf --config-name mlm_nsp_params
-    python -m ptls.pl_inference \
-        model_path="${hydra:runtime.cwd}/../../artifacts/scenario_alpha_battle/mlmnsp__$logger_name.p" \
-        output.path="${hydra:runtime.cwd}/data/emb_mlmnsp__$logger_name" \
+
+      python -m ptls.pl_inference \
+        trainer.gpus=[${device}] \
+        pl_module.hidden_size=${hidden_size} \
+        pl_module.seq_encoder.num_attention_heads=${n_heads} \
+        pl_module.seq_encoder.num_hidden_layers=${n_layers} \
+        inference.batch_size=256 \
+        model_path="models/mlmnsp__$logger_name.p" \
+        embed_file_name="emb_mlmnsp__${logger_name}" \
       --config-dir conf --config-name mlm_nsp_params
+
 done <<< $tuning_set
 
-# Compare
-rm results/scenario_alpha_battle__hidden_size.txt
+# # Compare
+# rm results/scenario_alpha_battle_new.txt
 # rm -r conf/embeddings_validation.work/
-python -m embeddings_validation \
-    --config-dir conf --config-name embeddings_validation_short +workers=10 +total_cpu_count=20 \
-    report_file="${hydra:runtime.cwd}/results/scenario_alpha_battle__hidden_size.txt" \    
-    auto_features=["${hydra:runtime.cwd}/data/emb_mlmnsp__*.pickle"]
+# python -m embeddings_validation \
+#     --config-dir conf --config-name embeddings_validation_short +workers=10 +total_cpu_count=20 \
+#     ++report_file="results/scenario_alpha_battle_new.txt" 
+    
 
