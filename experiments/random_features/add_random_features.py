f50513328a3fdb964a2a43bca51987e01cb1a208
Artyom Vorobev <artemvorobev2@gmail.com>
2022-12-28 12:56:40 +0000

    optimized add random features

--- experiments/random_features/add_random_features.py
+++ experiments/random_features/add_random_features.py
@@ -1,39 +1,26 @@
-import os
-import yaml
-import argparse
+import hydra
 import pandas as pd
 import numpy as np
+from omegaconf import DictConfig, OmegaConf
 
 
-def parse_args(args=None):
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--conf_path', type=os.path.abspath, default="random_features.yaml")
-    args = parser.parse_args(args)
-
-    with open(args.conf_path, 'rb') as f:
-        conf = yaml.load(f.read(), Loader=yaml.FullLoader)
-
-    return conf
-
-def add_features(config):
+@hydra.main(version_base=None, config_path=".", config_name="random_features")
+def add_features(config: DictConfig):
     def add_random_features(df, features, user_id_col):
         df_ids = df[user_id_col].unique()
         for feature, values in features.items():
-            df[feature] = np.nan
-            for user_id in df_ids:
-                df[feature][df[user_id_col] == user_id] = np.random.randint(low=values["min"], high=values["max"])
+            random_column = pd.Series(np.random.randint(low=values["min"], high=values["max"], size=len(df_ids)), index=df_ids)
+            df[feature] = random_column.reindex(df[user_id_col].values).values
         return df
 
-    def add_arange_features(df, user_id_col, date_col, cycled_arange):
-        df["arange"] = np.nan
-        df['date'] = pd.to_datetime(df[date_col], format="%d%b%y:%H:%M:%S")
-        df = df.sort_values(by=['date'])
-        for user_id in df[user_id_col].unique():
-            if cycled_arange:
-                df.arange[df[user_id_col] == user_id] = np.arange(len(df[df[user_id_col] == user_id]), dtype=int)%100 + 1
-            else:
-                df.arange[df[user_id_col] == user_id] = np.arange(len(df[df[user_id_col] == user_id]), dtype=int)
+    def add_arange_features(df, user_id_col, date_col, cycled_arange, datetime_format):
+        if datetime_format:
+            df['date'] = pd.to_datetime(df[date_col], format=datetime_format)
+            df = df.sort_values(by=['date'])
+        if cycled_arange:
+            df['arange'] = df.groupby([user_id_col]).cumcount()%100 + 1
+        else:
+            df['arange'] = df.groupby([user_id_col]).cumcount() + 1
         df = df.sort_index()
         return df 
         
@@ -45,8 +32,8 @@ def add_features(config):
         train = add_random_features(train, config["random_features"], config["user_id_col"])
         test = add_random_features(test, config["random_features"], config["user_id_col"])
     if config["add_arange_features"]:
-        train = add_arange_features(train, config["user_id_col"], config["date_col"], config["cycled_arange"])
-        test = add_arange_features(test, config["user_id_col"], config["date_col"], config["cycled_arange"])
+        train = add_arange_features(train, config["user_id_col"], config["date_col"], config["cycled_arange"], config["datetime_format"])
+        test = add_arange_features(test, config["user_id_col"], config["date_col"], config["cycled_arange"], config["datetime_format"])
 
     # print(len(train['arange'].unique()))
     train.to_csv(config["train_out"])
@@ -54,5 +41,4 @@ def add_features(config):
 
 
 if __name__ == '__main__':
-    config = parse_args()
-    add_features(config)
+    add_features()
