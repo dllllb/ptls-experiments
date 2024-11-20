8303b8af546ad74f35706c34b35051945b4557a7
Omar Zoloev <zoloevomarwork@gmail.com>
2024-11-20 17:30:04 +0300

    Update get-data.sh

--- scenario_bowl2019/bin/get-data.sh
+++ scenario_bowl2019/bin/get-data.sh
@@ -3,8 +3,8 @@
 mkdir data
 cd data
 
-curl -o train.csv 'https://huggingface.co/datasets/dllllb/datascience-bowl2019/resolve/main/train.csv.gz?download=true'
-curl -o test.csv 'https://huggingface.co/datasets/dllllb/datascience-bowl2019/resolve/main/test.csv.gz?download=true'
-curl -o train_labels.csv 'https://huggingface.co/datasets/dllllb/datascience-bowl2019/resolve/main/train_labels.csv.gz?download=true'
+curl -o train.csv -L 'https://huggingface.co/datasets/dllllb/datascience-bowl2019/resolve/main/train.csv.gz?download=true'
+curl -o test.csv -L 'https://huggingface.co/datasets/dllllb/datascience-bowl2019/resolve/main/test.csv.gz?download=true'
+curl -o train_labels.csv -L 'https://huggingface.co/datasets/dllllb/datascience-bowl2019/resolve/main/train_labels.csv.gz?download=true'
 
 gunzip -f *.csv.gz
