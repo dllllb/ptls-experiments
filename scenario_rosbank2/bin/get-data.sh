2c02e73e3fbe0176f7a2304dc4470f78c12b3f72
Omar Zoloev <zoloevomarwork@gmail.com>
2024-11-20 12:34:36 +0300

    Update get-data.sh

--- scenario_rosbank2/bin/get-data.sh
+++ scenario_rosbank2/bin/get-data.sh
@@ -3,7 +3,7 @@
 mkdir data
 cd data
 
-curl -OL 'https://huggingface.co/datasets/dllllb/rosbank-churn/resolve/main/train.csv.gz?download=true'
-curl -OL 'https://huggingface.co/datasets/dllllb/rosbank-churn/resolve/main/test.csv.gz?download=true'
+curl -o train.csv 'https://huggingface.co/datasets/dllllb/rosbank-churn/resolve/main/train.csv.gz?download=true'
+curl -o test.csv 'https://huggingface.co/datasets/dllllb/rosbank-churn/resolve/main/test.csv.gz?download=true'
 
 gunzip -f *.csv.gz
