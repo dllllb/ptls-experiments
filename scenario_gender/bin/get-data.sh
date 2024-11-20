86e73815abd2cedf994d635a2462ccb5ae151fcd
Omar Zoloev <zoloevomarwork@gmail.com>
2024-11-20 17:30:13 +0300

    Update get-data.sh

--- scenario_gender/bin/get-data.sh
+++ scenario_gender/bin/get-data.sh
@@ -3,7 +3,7 @@
 mkdir data
 cd data
 
-curl -o gender_train.csv 'https://huggingface.co/datasets/dllllb/transactions-gender/resolve/main/gender_train.csv?download=true'
-curl -o transactions.csv 'https://huggingface.co/datasets/dllllb/transactions-gender/resolve/main/transactions.csv.gz?download=true'
+curl -o gender_train.csv -L 'https://huggingface.co/datasets/dllllb/transactions-gender/resolve/main/gender_train.csv?download=true'
+curl -o transactions.csv -L 'https://huggingface.co/datasets/dllllb/transactions-gender/resolve/main/transactions.csv.gz?download=true'
 
 gunzip -f *.csv.gz
